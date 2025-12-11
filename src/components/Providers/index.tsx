9a4eb333f4a49603082ec16191e80df26a01d0a9
Martin Šíl <mail@martinsil.cz>
2025-12-11 21:25:12 +0100

    feat: add Sonner toaster to Providers component

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,7 +1,14 @@
+import { Toaster } from "../ui/sonner";
+
 type Props = {
 	children: React.ReactNode;
 };
 
 export default function Providers({ children }: Props) {
-	return children;
+	return (
+		<>
+			{children}
+			<Toaster position="top-center" />
+		</>
+	);
 }
