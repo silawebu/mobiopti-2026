311873c02f1fb889be820b59782f3e8a55f4c63a
Martin Šíl <mail@martinsil.cz>
2025-12-15 19:05:10 +0100

    fix: simplify dashboard page

--- src/app/(dashboard)/(main)/dashboard/page.tsx
+++ src/app/(dashboard)/(main)/dashboard/page.tsx
@@ -1,10 +1,8 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import SubscribeButton from "./_components/Subscribe/SubscribeButton";
 import { hasPlan } from "@/utils/subscription";
 import Subscribe from "./_components/Subscribe";
-import NewProject from "./_components/NewProject";
 
 export default async function Dashboard() {
 	const session = await auth.api.getSession({
@@ -20,30 +18,8 @@ export default async function Dashboard() {
 	return (
 		<section className="w-full flex items-center justify-center py-10">
 			<div className="max-w-[1000px] w-full px-2">
-				{!isSubscribed ? (
-					<>
-						<h1 className="font-bold text-3xl pb-5">
-							Hello, you are in dashboard
-						</h1>
-						<Subscribe />
-					</>
-				) : (
-					<>
-						<div className="flex items-center w-full justify-between">
-							<h1 className="font-bold text-3xl pb-5">Your projects</h1>
-							<NewProject />
-						</div>
-					</>
-				)}
-				{/* {urls && urls.length > 0 ? (
-					<div className="flex py-5 flex-col gap-3">
-						{urls.map((u: UrlWithScore, index: number) => (
-							<LinkCard key={index} {...u} />
-						))}
-					</div>
-				) : (
-					<span>You havent added any links yet</span>
-				)} */}
+				<h1 className="font-bold text-3xl pb-5">Welcome to MobiOpti</h1>
+				{!isSubscribed && <Subscribe />}
 			</div>
 		</section>
 	);
