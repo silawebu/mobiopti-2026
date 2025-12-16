739787952c3bd7af9d1a0a258db65f47885bbb8e
Martin Šíl <mail@martinsil.cz>
2025-12-16 20:23:56 +0100

    chore: switch to dark mode as default theme

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -9,7 +9,7 @@ export default function Providers({ children }: Props) {
 	return (
 		<ThemeProvider
 			attribute="class"
-			defaultTheme="system"
+			defaultTheme="dark"
 			enableSystem
 			disableTransitionOnChange
 		>
