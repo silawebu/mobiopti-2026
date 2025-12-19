9a1b9cda4e6999b39e689403da73e72080c77d48
Martin Šíl <mail@martinsil.cz>
2025-12-19 01:02:29 +0100

    fix: padding on link detail layout

--- src/app/(dashboard)/(main)/dashboard/links/[linkId]/layout.tsx
+++ src/app/(dashboard)/(main)/dashboard/links/[linkId]/layout.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function LinkLayout({ children }: Props) {
 	return (
-		<div className="w-full flex justify-center pt-10">
+		<div className="w-full flex justify-center py-10">
 			<div className="w-full max-w-4xl flex flex-col gap-4">
 				<div>
 					<GoBack text="Back to all links" href="/dashboard/links" />
