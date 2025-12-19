30890ac1464705b7db44a05ade026e81a695eddc
Martin Šíl <mail@martinsil.cz>
2025-12-19 01:03:12 +0100

    fix: change paddings on page layout of all links page

--- src/app/(dashboard)/(main)/dashboard/links/_components/PageLayout.tsx
+++ src/app/(dashboard)/(main)/dashboard/links/_components/PageLayout.tsx
@@ -2,12 +2,15 @@ import Heading from "./Heading";
 
 type Props = {
 	children: React.ReactNode;
-   actionDisabled?: boolean;
+	actionDisabled?: boolean;
 };
 
-export default function PageLayout({ children, actionDisabled = false }: Props) {
+export default function PageLayout({
+	children,
+	actionDisabled = false,
+}: Props) {
 	return (
-		<div className="w-full flex justify-center pt-10">
+		<div className="w-full flex justify-center py-10">
 			<div className="w-full max-w-4xl flex flex-col gap-10">
 				<Heading actionDisabled={actionDisabled} />
 				{children}
