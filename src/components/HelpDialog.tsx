e5aea7b5ba7e218aa53280fbe6824f67df488465
Martin Šíl <mail@martinsil.cz>
2025-12-21 15:28:15 +0100

    feat: create a reusable help dialog component

---
+++ src/components/HelpDialog.tsx
@@ -0,0 +1,36 @@
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+type Props = {
+	children: React.ReactNode;
+	label: string;
+	text: string | React.ReactNode;
+};
+
+export default function HelpDialog({ children, label, text }: Props) {
+	return (
+		<AlertDialog>
+			<AlertDialogTrigger>{children}</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>{label}</AlertDialogTitle>
+					<AlertDialogDescription asChild>
+						<div>{text}</div>
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Close</AlertDialogCancel>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+}
