8330fa34d0fda6846c74a65888a5a4d8fe1fb184
Martin Šíl <mail@martinsil.cz>
2025-12-22 18:45:55 +0100

    feat: move landing page to public marketing segment

--- src/app/(public)/(marketing)/page.tsx
+++ src/app/(public)/(marketing)/page.tsx
@@ -1,6 +1,6 @@
-import HomePageRedirector from "./_components/HomePageRedirector";
+import HomePageRedirector from "../_components/HomePageRedirector";
 import BrandIcon from "@/components/Icons/BrandIcon";
-import HeroSection from "./_components/Sections/HeroSection";
+import HeroSection from "../_components/Sections/HeroSection";
 
 export default function HomePage() {
 	return (
