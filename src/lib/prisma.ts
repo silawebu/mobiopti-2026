1c73b4a77ff70d4b2e06f3d86adfc26f8ec30de7
Martin Šíl <mail@martinsil.cz>
2026-01-04 18:38:17 +0100

    fix: production errors vol 1

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,17 @@
-import { PrismaClient } from "@/generated/prisma/client";
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/generated/prisma/client";
 
 const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL!,
+	connectionString: process.env.DATABASE_URL,
 });
 
-const globalForPrisma = global as unknown as {
-	prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+	prisma: PrismaClient | undefined;
 };
 
-const prisma =
-	globalForPrisma.prisma ||
-	new PrismaClient({
-		adapter,
-	});
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") {
+	globalForPrisma.prisma = prisma;
+}
